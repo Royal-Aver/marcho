7149f78802946b4f7e6676bb292c171f4c663216
Royal_Aver <royal_aver@rambler.ru>
2021-06-19 12:29:51 +0300

    added div promo

--- app/js/main.js
+++ app/js/main.js
@@ -15,4 +15,47 @@ $(function () {
 		readOnly: true,
 	});
 
+	function getTimeRemaining(endtime) {
+		const total = Date.parse(endtime) - Date.parse(new Date());
+		const seconds = Math.floor((total / 1000) % 60);
+		const minutes = Math.floor((total / 1000 / 60) % 60);
+		const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+		const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+		return {
+			total,
+			days,
+			hours,
+			minutes,
+			seconds
+		};
+	}
+
+	function initializeClock(id, endtime) {
+		const clock = document.querySelector('.clock');
+		const daysSpan = clock.querySelector('.clock__days');
+		const hoursSpan = clock.querySelector('.clock__hours');
+		const minutesSpan = clock.querySelector('.clock__minutes');
+		const secondsSpan = clock.querySelector('.clock__seconds');
+
+		function updateClock() {
+			const t = getTimeRemaining(endtime);
+
+			daysSpan.innerHTML = t.days;
+			hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+			minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+			secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+			if (t.total <= 0) {
+				clearInterval(timeinterval);
+			}
+		}
+
+		updateClock();
+		const timeinterval = setInterval(updateClock, 1000);
+	}
+
+	const deadline = $('.clock').attr('data-time');
+	initializeClock('clock', deadline);
+
 });
